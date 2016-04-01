26ebede0a6b4cdbffc4e30091da40bb75515b4d7
Thayne McCombs <astrothayne@gmail.com>
2016-03-31 21:52:44 -0600

    Add rna-transcription exercise

---
+++ exercises/rna-transcription/rna_test.d
@@ -0,0 +1,19 @@
+import std.exception : assertThrown;
+import std.stdio;
+
+import rna;
+
+void main() {
+    assert(dnaComplement("C") == "G");
+    assert(dnaComplement("G") == "C");
+    assert(dnaComplement("T") == "A");
+    assert(dnaComplement("A") == "U");
+
+    assert(dnaComplement("ACGTGGTCTTAA") == "UGCACCAGAAUU");
+
+    assertThrown(dnaComplement("U"));
+    assertThrown(dnaComplement("XXX"));
+    assertThrown(dnaComplement("ACGTXXXCTTAA"));
+
+    writeln("All tests passed");
+}
