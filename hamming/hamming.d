8c4be493740afbd32ed546ed147b75a7440c5686
Marius Cetateanu <mce@softkinetic.com>
2016-07-13 16:28:31 +0200

    Add Hamming problem.

---
+++ hamming/hamming.d
@@ -0,0 +1,27 @@
+
+module hamming;
+
+
+unittest
+{
+const int all_tests_enabled = 0;
+
+	assert(distance("A", "A") == 0);
+static if (all_tests_enabled)
+{
+    assert(distance("A", "G") == 1);
+    assert(distance("AG", "CT") == 2);
+    assert(distance("AT", "CT") == 1);
+    assert(distance("GGACG", "GGTCG") == 1);
+    assertThrown(distance("AAAG", "AAA"));
+    assertThrown(distance("AAA", "AAAG"));
+    assert(distance("GATACA", "GCATAA") == 4);
+    assert(distance("GGACGGATTCTG", "AGGACGGATTCT") == 9);
+}
+
+}
+
+void main ()
+{
+}
+
