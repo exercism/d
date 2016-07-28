b66d2c1bd4db955a8805811071f5fb5accca035e
Marius Cetateanu <mce@softkinetic.com>
2016-07-28 16:29:36 +0200

    Recover implementation.

--- exercises/crypto-square/example.d
+++ exercises/crypto-square/example.d
@@ -1,4 +1,103 @@
-/ normalize_strange_characters
+
+module crypto;
+
+import std.string;
+import std.array;
+import std.math : sqrt, floor, ceil;
+import std.conv : to;
+
+import std.stdio;
+
+
+class cipher
+{
+public:
+	this (const string text)
+	{
+		this.normalized =  toLower(text.removechars(" `#$%^&.,<>:;'!~-?*@()[]{}_=|/\t\a\n\"+"));
+	}
+
+	string normalizePlainText ()
+	{
+		return normalized;
+	}
+
+	/**
+	* Return the number of encrypted segments(that is number of columns for the crypto square).
+	*/
+	size_t size ()
+	{
+		size_t root = to!ulong(floor(sqrt(to!float(normalized.length))));
+
+		size_t distance = normalized.length - root * root;
+		if (distance == 0)
+		{
+			return root;
+		}
+		else
+		{
+			return to!ulong(root) + 1;
+		}
+	}
+
+	string[] plainTextSegments ()
+	{
+		size_t columns = size();
+		size_t rem = normalized.length % columns;
+		string[] result;
+
+		size_t len = normalized.length;
+		for (size_t j = 0; j <= (len - columns); j += columns)
+		{
+			result ~= normalized[j..j + columns];
+		}
+
+		// we need to handle the leftovers
+		if (rem)
+		{
+			result ~= normalized[len - rem .. len];
+		}
+
+		return result;
+	}
+
+	string cipherText ()
+	{
+		string result = normalizedCipherText();
+
+		return result.removechars(" ");
+	}
+
+	string normalizedCipherText ()
+	{
+		string result;
+		string[] segments = plainTextSegments();
+		size_t columns = size();
+
+		for (size_t i = 0; i < columns; ++i)
+		{
+			string cryptoSegment;
+			foreach (s; segments)
+			{
+				if (i < s.length) cryptoSegment ~= s[i];
+			}
+
+			result ~= cryptoSegment;
+			result ~= " ";
+		}
+
+		return strip(result);
+
+	}
+
+private:
+	const string normalized;
+}
+
+unittest
+{
+
+// normalize_strange_characters
 {
 	auto theCipher = new cipher("s#$%^&plunk");
 	assert("splunk" == theCipher.normalizePlainText());
