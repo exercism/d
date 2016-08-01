209b735346069b17443d830108f87a208d705fbc
Marius Cetateanu <mce@softkinetic.com>
2016-08-01 15:59:41 +0200

    Complete circular buffer problem.

--- exercises/circular-buffer/circular.d
+++ exercises/circular-buffer/circular.d
@@ -2,100 +2,19 @@
 module circular;
 
 import std.exception;
-import std.stdio;
-import std.string;
-
-class Buffer(ElementType)
-{
-public:
-	this (size_t capacity)
-	{
-		container.length = capacity;
-		unread = 0;
-		readIndex = 0;
-		writeIndex = 0;
-		this.capacity = capacity;
-	}
-
-	ElementType pop ()
-	{
-		if (empty())
-		{
-			throw new Exception(format("Popped empty buffer!"));
-		}
-
-		ElementType last = container[readIndex];
-		readIndex = (readIndex + 1) % capacity;
-		--unread;
-
-		return last;
-	}
-
-	void push (ElementType element)
-	{
-		if (full())
-		{
-			throw new Exception(format("Pushed into full buffer!"));
-		}
-
-		container[writeIndex] = element;
-		writeIndex = (writeIndex + 1) % capacity;
-		++unread;
-	}
-
-	void clear ()
-	{
-		unread = 0;
-		readIndex = 0;
-		writeIndex = 0;
-	}
-
-	void force_push (ElementType element)
-	{
-		if (this.full())
-		{
-			container[readIndex] = element;
-			readIndex = advanceIndex(readIndex);
-			writeIndex = readIndex;
-		}
-		else
-		{
-			push(element);
-		}
-	}
-
-	bool empty ()
-	{
-		return unread == 0;
-	}
-
-	bool full ()
-	{
-		return unread == capacity;
-	}
-
-private:
-	size_t advanceIndex (size_t index)
-	{
-		return (index + 1) % capacity;
-	}
-
-	ElementType[] container;
-	int unread;
-	size_t readIndex;
-	size_t writeIndex;
-	const size_t capacity;
-}
-
 
 unittest
 {
+immutable int allTestsEnabled = 0;
 
 // test read empty buffer
 {
 	auto myBuffer = new Buffer!(int)(1UL);
 	assertThrown(myBuffer.pop(), "Empty buffer should throw exception if popped!");
 }
+
+static if (allTestsEnabled)
+{
 
 // test write and read back one item
 {
@@ -203,6 +122,8 @@ unittest
 	assert(myBuffer.pop() == 'A');
 	assert(myBuffer.pop() == 'B');
 	assertThrown(myBuffer.pop(), "Empty buffer should throw exception if popped!");
+}
+
 }
 
 }
