092aa052a8a3e3ec5a59b1ac7bab80a17197064b
Katrina Owen <kytrinyx@github.com>
2016-08-13 11:38:10 -0600

    Normalize gigasecond filenames

--- exercises/gigasecond/gigasecond.d
+++ exercises/gigasecond/gigasecond.d
@@ -1,8 +1,21 @@
-module gigasecond;
-
+import std.stdio;
 import std.datetime;
 
-DateTime gsAnniversary(DateTime start) {
-    // Your code here
-    return start;
+import gigasecond;
+
+void main() {
+    assert(gsAnniversary(DateTime(2011, 4, 25)) == DateTime(2043, 1, 1, 1, 46, 40));
+    assert(gsAnniversary(DateTime(1977, 6, 13)) == DateTime(2009, 2, 19, 1, 46, 40));
+    assert(gsAnniversary(DateTime(1959, 7, 19)) == DateTime(1991, 3, 27, 1, 46, 40));
+    assert(gsAnniversary(DateTime(2015, 1, 24, 22, 0, 0)) == DateTime(2046, 10, 2, 23, 46, 40));
+    assert(gsAnniversary(DateTime(2015, 1, 24, 23, 59, 59)) == DateTime(2046, 10, 3, 1, 46, 39));
+
+    //check that it doesn't mutate the argument
+    auto d = DateTime(2011, 4, 25);
+    assert(gsAnniversary(d) == DateTime(2043, 1, 1, 1, 46, 40));
+    assert(d == DateTime(2011, 4, 25));
+
+    //For fun add a test for your own gigasecond anniversary
+
+    writeln("All tests pass");
 }
