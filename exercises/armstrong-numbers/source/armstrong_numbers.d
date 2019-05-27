068e19d16bbef9b0097640664cadecdd5a27dc33
Anthony Scotti <anthony.m.scotti@gmail.com>
2019-05-27 15:07:30 -0400

    Adds armstrong-numbers exercise

---
+++ exercises/armstrong-numbers/source/armstrong_numbers.d
@@ -0,0 +1,36 @@
+module armstrong_numbers;
+
+unittest
+{
+    immutable int allTestsEnabled = 0;
+
+    // Zero is an Armstrong number
+    assert(isArmstrongNumber(0));
+
+    static if (allTestsEnabled)
+    {
+        // Single digit numbers are Armstrong numbers
+        assert(isArmstrongNumber(5));
+
+        // There are no 2 digit Armstrong numbers
+        assert(!isArmstrongNumber(10));
+
+        // Three digit number that is an Armstrong number
+        assert(isArmstrongNumber(153));
+
+        // Three digit number that is not an Armstrong number
+        assert(!isArmstrongNumber(100));
+
+        // Four digit number that is an Armstrong number
+        assert(isArmstrongNumber(9474));
+
+        // Four digit number that is not an Armstrong number
+        assert(!isArmstrongNumber(9475));
+
+        // Seven digit number that is an Armstrong number
+        assert(isArmstrongNumber(9926315));
+
+        // Seven digit number that is not an Armstrong number
+        assert(!isArmstrongNumber(9926314));
+    }
+}
