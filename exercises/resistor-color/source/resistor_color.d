7a67959fc5e9dd0ef043541380868a38619ee4a8
Anthony Scotti <anthony.m.scotti@gmail.com>
2019-05-28 19:30:52 -0400

    Adds resistor-color exercise

---
+++ exercises/resistor-color/source/resistor_color.d
@@ -0,0 +1,25 @@
+module resistor_color;
+
+unittest
+{
+    immutable int allTestsEnabled = 0;
+
+    // Black
+    assert(ResistorColor.colorCode("black") == 0);
+
+    static if (allTestsEnabled)
+    {
+        // White
+        assert(ResistorColor.colorCode("white") == 9);
+
+        // Orange
+        assert(ResistorColor.colorCode("orange") == 3);
+
+        // Colors
+        assert(ResistorColor.colors == [
+                "black", "brown", "red", "orange", "yellow", "green", "blue",
+                "violet", "grey", "white"
+                ]);
+    }
+
+}
