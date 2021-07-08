e0e6d5f2e968928ab8dfce3f89a1b28ba3260eff
Peter Tseng <pht24@cornell.edu>
2021-07-08 04:02:24 +0000

    Revert "proof: fail to compile"
    This reverts commit 2db948e201e4b478e81af16acd88daac080f6664.

--- exercises/practice/armstrong-numbers/example/armstrong_numbers.d
+++ exercises/practice/armstrong-numbers/example/armstrong_numbers.d
@@ -5,7 +5,6 @@ import std.conv : to;
 
 pure bool isArmstrongNumber(immutable int number)
 {
-  this doesn't compile.
     immutable string s = number.to!string;
     immutable auto sum = s.map!(a => a.to!string
             .to!int ^^ s.length).sum;
