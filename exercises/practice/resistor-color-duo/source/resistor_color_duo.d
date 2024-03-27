e52e88b2c9dd20b5d8a177bb6a68f7a7d6ef7042
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2024-03-27 06:50:42 -0700

    Update resistor-color-duo tests (#226)

--- exercises/practice/resistor-color-duo/source/resistor_color_duo.d
+++ exercises/practice/resistor-color-duo/source/resistor_color_duo.d
@@ -1,5 +1,13 @@
 module resistor_color_duo;
 
+class ResistorColorDuo
+{
+    static pure long value(immutable string[] colorsInput)
+    {
+        // implement this function
+    }
+}
+
 unittest
 {
     immutable int allTestsEnabled = 0;
@@ -15,11 +23,17 @@ unittest
         // Yellow and violet
         assert(ResistorColorDuo.value(["yellow", "violet"]) == 47);
 
+        // White and red
+        assert(ResistorColorDuo.value(["white", "red"]) == 92);
+
         // Orange and orange
         assert(ResistorColorDuo.value(["orange", "orange"]) == 33);
 
         // Ignore additional colors
         assert(ResistorColorDuo.value(["green", "brown", "orange"]) == 51);
+
+        // Black and brown, one-digit
+        assert(ResistorColorDuo.value(["black", "brown"]) == 1);
     }
 
 }
