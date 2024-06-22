9ae423954c197fffc64ddc0e34d53a860b74d63a
keiravillekode <keiravillekode@gmail.com>
2024-06-23 00:58:53 +1000

    Sync exercise docs and metadata (#257)
    * Sync exercise docs and metadata
    
    [no important files changed]
    
    * Replace track-specific gigasecond test

--- exercises/practice/gigasecond/source/gigasecond.d
+++ exercises/practice/gigasecond/source/gigasecond.d
@@ -23,10 +23,10 @@ unittest
         // Full time with day roll-over
         assert(add(DateTime(2015, 1, 24, 23, 59, 59)) == DateTime(2046, 10, 3, 1, 46, 39));
 
-        // Check that it doesn't mutate the argument
-        auto d = DateTime(2011, 4, 25);
-        assert(add(d) == DateTime(2043, 1, 1, 1, 46, 40));
-        assert(d == DateTime(2011, 4, 25));
+        // Does not mutate the input
+        auto d = DateTime(2015, 1, 24, 23, 59, 59);
+        assert(add(d) == DateTime(2046, 10, 3, 1, 46, 39));
+        assert(d == DateTime(2015, 1, 24, 23, 59, 59));
     }
 
 }
